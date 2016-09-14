e6bfe469b96d610a6a721d73cec43fd1bb646dbb
NosovArtem <nosov.a.o@yandex.ru>
2016-09-14 11:47:14 +0300

    Lessonn2 created class Person

---
+++ src/ru/sberbank/school/helloworld/tasks/Lesson02/Person.java
@@ -0,0 +1,75 @@
+package ru.sberbank.school.helloworld.tasks.Lesson02;
+
+import java.util.Objects;
+
+public class Person {
+    private final boolean man;
+    private final String name;
+    private Person spouse;
+
+    public Person(boolean man, String name) {
+        this.man = man;
+        this.name = name;
+    }
+
+    /**
+     * This method checks gender of persons. If genders are not equal - tries to marry.
+     * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife.
+     * Example:
+     * if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
+     *
+     * @param person - new husband/wife for this person.
+     * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
+     */
+    public boolean marry(Person person) {
+        if (man == person.man) {
+            return false;
+        }
+        if (spouse != null && spouse.equals(person)) {
+            return false;
+        }
+        if (spouse != null) {
+            divorce();
+        }
+        if (person.spouse != null) {
+            person.divorce();
+        }
+        spouse = person;
+        person.spouse = this;
+        return true;
+    }
+
+    /**
+     * Sets spouse = null if spouse is not null
+     *
+     * @return true - if person status has been changed
+     */
+    public boolean divorce() {
+        if (spouse != null) {
+            spouse.spouse = null;
+            spouse = null;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Person)) {
+            return false;
+        }
+        Person that = (Person) o;
+        return Objects.equals(man, that.man) &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(man, name);
+    }
+
+}
