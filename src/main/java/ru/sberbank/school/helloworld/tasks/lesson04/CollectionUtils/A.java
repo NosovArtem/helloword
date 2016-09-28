8870f951ac0c1ef6bfcdca8b12abbe5c32f36fd2
NosovArtem <nosov.a.o@yandex.ru>
2016-09-28 20:35:39 +0300

    lesson06

---
+++ src/main/java/ru/sberbank/school/helloworld/tasks/lesson04/CollectionUtils/A.java
@@ -0,0 +1,7 @@
+package ru.sberbank.school.helloworld.tasks.lesson04.CollectionUtils;
+
+/**
+ * Created by Admin on 28.09.2016.
+ */
+public class A {
+}
--- src/main/java/ru/sberbank/school/helloworld/tasks/lesson04/CollectionUtils/CollectionUtils.java
+++ src/main/java/ru/sberbank/school/helloworld/tasks/lesson04/CollectionUtils/CollectionUtils.java
@@ -1,9 +1,6 @@
 package ru.sberbank.school.helloworld.tasks.lesson04.CollectionUtils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class CollectionUtils {
 
