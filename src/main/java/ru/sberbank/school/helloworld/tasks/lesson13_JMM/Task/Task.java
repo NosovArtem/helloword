17b2a2e31fcf1a778d44f3ab62cf5b2716d08628
NosovArtem <nosov.a.o@yandex.ru>
2016-10-31 13:35:46 +0300

    Jmm

--- src/main/java/ru/sberbank/school/helloworld/tasks/lesson13_JMM/Task/Task.java
+++ src/main/java/ru/sberbank/school/helloworld/tasks/lesson13_JMM/Task/Task.java
@@ -15,24 +15,22 @@ public class Task<T> {
 
     public T get() {
         System.out.println(Thread.currentThread().getName() + " вызвал метод get");
-        T temp;
         if (result == null) {
             System.out.println(Thread.currentThread().getName() + " для данног потока результат не известен");
             synchronized (this) {
                 System.out.println(Thread.currentThread().getName() + "  в области synchronized");
                 try {
-                    temp = callable.call();
-                    System.out.println(Thread.currentThread().getName() + " получил значение result: " + temp);
-                    result = temp;
+                    result = callable.call();
+                    System.out.println(Thread.currentThread().getName() + " получил значение result: " + result);
                 } catch (Exception e) {
                     throw new ExecutionRuntimeException("Ошибка во время выполнения расчета", e);
                 }
             }
         } else {
             System.out.println(Thread.currentThread().getName() + " Результат уже известен. Возвращаем результат без входа в область synchronized ");
-            temp = result;
+
         }
-        return temp;
+        return result;
     }
 
 }
