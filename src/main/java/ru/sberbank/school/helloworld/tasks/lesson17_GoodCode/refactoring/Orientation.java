90e47de7d2a514b9c879f875fdb3a356509cd3ab
NosovArtem <nosov.a.o@yandex.ru>
2016-11-13 19:41:28 +0300

    refactoring

---
+++ src/main/java/ru/sberbank/school/helloworld/tasks/lesson17_GoodCode/refactoring/Orientation.java
@@ -0,0 +1,6 @@
+package ru.sberbank.school.helloworld.tasks.lesson17_GoodCode.refactoring;
+
+public enum Orientation {
+
+    NORTH, WEST, SOUTH, EAST;
+}
---
+++ src/main/java/ru/sberbank/school/helloworld/tasks/lesson17_GoodCode/refactoring/TractorInDitchException.java
@@ -0,0 +1,5 @@
+package ru.sberbank.school.helloworld.tasks.lesson17_GoodCode.refactoring;
+
+public class TractorInDitchException extends RuntimeException {
+
+}
